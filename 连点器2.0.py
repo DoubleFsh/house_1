ec8e53be23b262577ba6159c6a1f213a34b9faed
DoubleFsh <95062868+DoubleFsh@users.noreply.github.com>
2022-01-18 15:58:20 +0800

    Add files via upload

---
+++ 连点器2.0.py
@@ -0,0 +1,71 @@
+from pynput import keyboard,mouse
+from pynput.keyboard import Key, Controller
+from pynput.mouse import Button, Controller
+
+mouse = Controller()
+
+from pynput import mouse
+
+def on_move(x, y):
+    print('Pointer moved to {0}'.format(
+        (x, y)))
+
+def on_click(x, y, button, pressed):
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
+
+def on_scroll(x, y, dx, dy):
+    print('Scrolled {0} at {1}'.format(
+        'down' if dy < 0 else 'up',
+        (x, y)))
+
+# Collect events until released
+with mouse.Listener(
+        on_move=on_move,
+        on_click=on_click,
+        on_scroll=on_scroll) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = mouse.Listener(
+    on_move=on_move,
+    on_click=on_click,
+    on_scroll=on_scroll)
+listener.start()
+
+#-----------------------------------------------------------
+
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+
+#------------------------------------------------------------
+
+#读取鼠标位置
+while True:
+    print('位置是{0}'.format(mouse.position))
+
